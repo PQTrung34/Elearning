890ff242119664c2af59af06e690c6af154b3ed7
pqt <pqtrung34@gmail.com>
2024-11-04 21:59:49 +0700

    add delete user

--- routes/user.route.ts
+++ routes/user.route.ts
@@ -1,6 +1,7 @@
 import express from 'express';
-import { registrationUser, activateUser, loginUser, logoutUser, updateAccessToken, getUserInfo, socialAuth, updateUserInfo, updateUserPassword, updateProfilePicture, getAllUsersAdmin, updateUserRole } from '../controllers/user.controller';
+import { registrationUser, activateUser, loginUser, logoutUser, updateAccessToken, getUserInfo, socialAuth, updateUserInfo, updateUserPassword, updateProfilePicture, getAllUsersAdmin, updateUserRole, deleteUser } from '../controllers/user.controller';
 import { authorizeRoles, isAutheticated } from '../middleware/auth';
+import { deleteModel } from 'mongoose';
 const userRouter = express.Router()
 
 userRouter.post('/registration', registrationUser);
@@ -26,5 +27,7 @@ userRouter.put('/update-user-avatar', isAutheticated, updateProfilePicture)
 userRouter.get('/get-all-users', isAutheticated, authorizeRoles("admin"), getAllUsersAdmin);
 
 userRouter.put('/update-user-role', isAutheticated, authorizeRoles("admin"), updateUserRole);
+
+userRouter.delete('/delete-user/:id', isAutheticated, authorizeRoles("admin"), deleteUser);
 
 export default userRouter;
