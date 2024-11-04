b707af3cbcef7bf5a369a40803d22fdf1c52879d
pqt <pqtrung34@gmail.com>
2024-11-04 21:19:56 +0700

    add update noti

--- routes/notificaton.route.ts
+++ routes/notificaton.route.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { authorizeRoles, isAutheticated } from "../middleware/auth";
-import { getNotifications } from "../controllers/notification.controller";
+import { getNotifications, updateNotification } from "../controllers/notification.controller";
 const notificationRouter = express.Router();
 
 notificationRouter.get("/get-all-notifications",isAutheticated, authorizeRoles("admin"), getNotifications);
+
+notificationRouter.put("/update-notification/:id",isAutheticated, authorizeRoles("admin"), updateNotification);
 
 export default notificationRouter;
