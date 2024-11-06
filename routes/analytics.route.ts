106fe44aaacbcfa5aaf15b78f9e21152fb6f0a2f
pqt <pqtrung34@gmail.com>
2024-11-06 20:54:50 +0700

    update and fix bug

--- routes/analytics.route.ts
+++ routes/analytics.route.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import { authorizeRoles, isAutheticated } from "../middleware/auth";
 import { getCourseAnalytics, getOrderAnalytics, getUserAnalytics } from "../controllers/analytics.controller";
+import { updateAccessToken } from "../controllers/user.controller";
 const analyticsRouter = express.Router();
 
-analyticsRouter.get('/get-user-analytics', isAutheticated, authorizeRoles("admin"), getUserAnalytics);
+analyticsRouter.get('/get-user-analytics', updateAccessToken, isAutheticated, authorizeRoles("admin"), getUserAnalytics);
 
-analyticsRouter.get('/get-course-analytics', isAutheticated, authorizeRoles("admin"), getCourseAnalytics);
+analyticsRouter.get('/get-course-analytics', updateAccessToken, isAutheticated, authorizeRoles("admin"), getCourseAnalytics);
 
-analyticsRouter.get('/get-order-analytics', isAutheticated, authorizeRoles("admin"), getOrderAnalytics);
+analyticsRouter.get('/get-order-analytics', updateAccessToken, isAutheticated, authorizeRoles("admin"), getOrderAnalytics);
 
 export default analyticsRouter;
