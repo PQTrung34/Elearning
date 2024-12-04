b8bcad8d0490b0f3962046e609a6e4281ae795bb
pqt <pqtrung34@gmail.com>
2024-12-04 22:32:18 +0700

    fix order

--- services/order.service.ts
+++ services/order.service.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Response } from "express";
 import { CatchAsyncError } from "../middleware/catchAsyncError";
 import OrderModel from "../models/order.model";
+import CourseModel from "../models/course.model";
+import userModel from "../models/user.model";
 
 // create new order
 export const newOrder = CatchAsyncError(async(data: any, res: Response) => {
@@ -14,8 +16,20 @@ export const newOrder = CatchAsyncError(async(data: any, res: Response) => {
 // get all orders
 export const getAllOrdersService = async(res: Response) => {
     const orders = await OrderModel.find().sort({createAt: -1});
+    const results = await Promise.all(
+        orders.map(async (order: any) => {
+            const course = await CourseModel.findById(order.courseId);
+            const user = await userModel.findById(order.userId);
+            return {
+                ...order.toObject(), // Chuyển Mongoose document thành object thông thường
+                coursePrice: course?.price || null,
+                userName: user?.name || "Unknown User",
+            };
+        })
+    );
+
     res.status(201).json({
         success: true,
-        orders
+        results
     })
 }
